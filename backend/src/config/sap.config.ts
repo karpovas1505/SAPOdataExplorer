7fe8d7e4ac89bbc1069799918af4c8b7c14f2f7d
Карпов Александр Сергеевич <Karpov_AS@powerm.ru>
2026-03-26 10:28:21 +0300

    Update SAP_BACKEND_CLIENT to 280 in env files

--- backend/src/config/sap.config.ts
+++ backend/src/config/sap.config.ts
@@ -8,7 +8,7 @@ export const sapConfig = {
   
   get backendHost() { return process.env.SAP_BACKEND_HOST || process.env.SAP_HOST || ''; },
   get backendPort() { return parseInt(process.env.SAP_BACKEND_PORT || process.env.SAP_PORT || '8000'); },
-  get backendClient() { return process.env.SAP_BACKEND_CLIENT || process.env.SAP_CLIENT || '270'; },
+  get backendClient() { return process.env.SAP_BACKEND_CLIENT || process.env.SAP_CLIENT || '280'; },
   get backendBaseUrl() { return `http://${process.env.SAP_BACKEND_HOST || process.env.SAP_HOST}:${process.env.SAP_BACKEND_PORT || process.env.SAP_PORT}`; },
   
   catalogService: '/sap/opu/odata/IWFND/CATALOGSERVICE;v=2',
